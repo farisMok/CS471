1b69cc18cdc6ae91b08fc9480805f13173ca0da2
farisMok <faris.mkq1@gmail.com>
2024-09-15 12:33:32 +0300

    update

--- WebLab/libiraryproject/apps/bookmodule/apps.py
+++ WebLab/libiraryproject/apps/bookmodule/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BookmoduleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookmodule'
+    name = 'apps.bookmodule'
--- WebLab/libiraryproject/libiraryproject/settings.py
+++ WebLab/libiraryproject/libiraryproject/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.bookmodule',
-    'apps.usermodule'
+    'apps.usermodule',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
